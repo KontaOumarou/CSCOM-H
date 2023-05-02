41b13a2289e741912cf7ecc904e162efb3fa33ea
KontaOumarou <127104942+KontaOumarou@users.noreply.github.com>
2023-05-02 04:35:33 +0000

    Add files via upload

---
+++ CSCOMH/CSCOM/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'index.html')
+def login(request):
+    return render(request, 'login.html')
+def register(request):
+    return render(request, 'register.html')
